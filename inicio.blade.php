900aa4ecbf64ee916ec21b522a58b5b6a70a851b
DianaAylin <diana.gonzalez2304@uppuebla.edu.mx>
2024-03-11 10:20:22 -0600

    Inicio

---
+++ inicio.blade.php
@@ -0,0 +1,12 @@
+@extends('principal')
+
+@section('contenido')
+
+<center><h1> <b> BABY <br> CLOUD </b></h1></center>
+        <br><br>
+        <center><img class="rounded" src = "{{asset('archivos/bebe.jpg')}}"
+        height = 300 width = 600></center>
+        <center><h2> <b> Fabricamos<br> alegrías que <br> visten</b></h2></center>
+
+
+@stop
